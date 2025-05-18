b3dad256199e5697c0e9e2294599cee6e3eb5f7e
Till <till.wadehn@gmail.com>
2025-05-18 17:39:14 +0200

    Update page.tsx

--- src/app/notes/[id]/page.tsx
+++ src/app/notes/[id]/page.tsx
@@ -1,7 +1,7 @@
 
 import { notFound } from "next/navigation";
 
-export default async function NoteDetailPage({ params }: { params: { id: string } }) {
+export default async function NoteDetailPage() {
 
   return (
     <div className="space-y-4">
